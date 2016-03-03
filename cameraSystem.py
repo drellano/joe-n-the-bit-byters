a89eb116cc8a8817a39a1e4e515b900fe562c08a
rmriales <rmriales@memphis.edu>
2016-03-02 19:10:02 -0600

    Updated cameraSystem.py

--- cameraSystem.py
+++ cameraSystem.py
@@ -4,20 +4,25 @@ import RPi.GPIO as io
 io.setmode(io.BCM)
 
 pir_pin = 18
-lockLow = false
+lockLow = False
+oldtime = time.time();
 
 io.setup(pir_pin, io.IN)
 
+print("Calibrating motion sensor.")
+
 
 while True:
-
-	if io.input(pir_pin && Not(lockLoW)):
+	if io.input(pir_pin) and Not(lockLow):
+		print("Motion Detected!")
 		with picamera.PiCamera() as camera:
 			camera.resolution = (1024, 768)
 			camera.start_preview()
 			time.sleep(2)
 			camera.capture('testPic.jpg')
-		lockLow = true
-	elseif(time == 60000)
-		lockLow = false
-	
+		lockLow = True
+		oldtime = time.time()
+	elif(time.time() - oldtime >= 60) and lockLow:
+		print("Unlocking")
+		lockLow = False
+		oldtime = time.time()	
